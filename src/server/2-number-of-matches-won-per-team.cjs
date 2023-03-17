ef106977bb44a59a734389c4902ac91b6856758c
rahulbhardwaj <rahul.bhardwaj.23.3@mountblue.tech>
2023-03-17 13:21:05 +0530

    modified ques2 for penalties

--- src/server/2-number-of-matches-won-per-team.cjs
+++ src/server/2-number-of-matches-won-per-team.cjs
@@ -5,38 +5,57 @@ function numberOfMatchesWonPerTeam(worldCupMatches){
         return matchesWonPerTeam;
     }
 
-    function updateMatchesWonPerTeam(winTeam){
-        if(!matchesWonPerTeam[winTeam]){
-            matchesWonPerTeam[winTeam] = 1;
+    for(let matches of worldCupMatches){
+        if(matches['Home Team Name'] === ''){
+            continue;
         }
-        else{
-            matchesWonPerTeam[winTeam]+=1;
+        matchesWonPerTeam[matches['Home Team Name']] = 0;
+        matchesWonPerTeam[matches['Away Team Name']] = 0;
+    }
+
+    function getWinTeamFromPenalties(winCondition,matches){
+        if(winCondition === ' '){
+            return
         }
+        let homeTeamGoals = winCondition.split(' ');
+        homeTeamGoals = parseInt(homeTeamGoals[homeTeamGoals.length - 4][1]);
+
+        let awayTeamGoals = winCondition.split(' ');
+        awayTeamGoals = parseInt(awayTeamGoals[awayTeamGoals.length - 2][0]);
+
+        if(homeTeamGoals > awayTeamGoals){
+            return matches['Home Team Name'];
+        }
+        if(homeTeamGoals < awayTeamGoals){
+            return matches['Away Team Name'];
+        }
+        return;
     }
 
     for(let matches of worldCupMatches){
-        if(matches.Year === ''){
+        if(matches['Home Team Name'] === ''){
             //Ignoring Missing Values
             continue;
         }
 
         let winTeam;
+
         const homeTeamGoals = parseInt(matches['Home Team Goals']);
         const awayTeamGoals = parseInt(matches['Away Team Goals']);
         
-        if(homeTeamGoals>awayTeamGoals){
+        if(homeTeamGoals > awayTeamGoals){
             winTeam = matches['Home Team Name'];
         }
-        else if(homeTeamGoals<awayTeamGoals){
+        else if(homeTeamGoals < awayTeamGoals){
             winTeam = matches['Away Team Name'];
         }
-        else{
-            // Taking first word of penalties score are given in win conditions as France win on penalties (3 - 4)
-            winTeam = matches['Win conditions'].split(' ')[0];
+        else if(homeTeamGoals === awayTeamGoals){
+            winTeam = getWinTeamFromPenalties(matches['Win conditions'],matches);
         }
 
-        updateMatchesWonPerTeam(winTeam);
-
+        if(winTeam){
+            matchesWonPerTeam[winTeam]+=1;
+        }
     }
     
     return matchesWonPerTeam;
