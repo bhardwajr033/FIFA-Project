cd8fa83fd03643bbc5948364c3f9721f1368cb38
rahulbhardwaj <rahul.bhardwaj.23.3@mountblue.tech>
2023-04-01 11:20:16 +0530

    all 4 graphs rendered

--- index.js
+++ index.js
@@ -18,8 +18,28 @@ app.get("/style.css", (req, res) => {
     res.sendFile(path.resolve("public/style.css"));
 });
 
+app.get("/graphLabels", (req,res)=>{
+    fs.readFile('./public/graphLabels.json','utf8')
+    .then(data => res.send(data)); 
+})
+
 app.get("/matchesPerCity",(req,res)=>{
     fs.readFile('./src/public/output/1-number-of-matches-played-per-city.json','utf8')
+.then(data => res.send(data));
+})
+
+app.get("/matchesWonPerTeam",(req,res)=>{
+    fs.readFile('./src/public/output/2-number-of-matches-won-per-team.json','utf8')
+.then(data => res.send(data));
+})
+
+app.get("/redCardsPerTeamIn2014",(req,res)=>{
+    fs.readFile('./src/public/output/3-red-cards-issued-per-team-in-2014.json','utf8')
+.then(data => res.send(data));
+})
+
+app.get("/probabilityOfTopTenPlayers",(req,res)=>{
+    fs.readFile('./src/public/output/4-top-10-players-with-highest-probability-scoring-a-goal-in-a-match.json','utf8')
 .then(data => res.send(data));
 })
 
--- public/app.js
+++ public/app.js
@@ -1,53 +1,63 @@
+async function getProperty(property) {
+  const data = await fetch(`http://localhost:3000/${property}`).then((res) =>
+    res.json()
+  );
+  const graphLabels = await fetch(`http://localhost:3000/graphLabels`).then(
+    (res) => res.json()
+  );
+  buildGraph(data, graphLabels, property);
+}
 
-fetch('http://localhost:3000/matchesPerCity')
-.then(res => res.json())
-.then(data => {buildGraph(data)});
-
-function buildGraph(jsonData){
-    Highcharts.chart('container', {
-        chart: {
-          type: 'column'
+function buildGraph(jsonData, graphLabels, property) {
+  Highcharts.chart("container", {
+    chart: {
+      type: "column",
+    },
+    title: {
+      text: graphLabels[property].title,
+    },
+    xAxis: {
+      type: "category",
+      title: {
+        text: graphLabels[property].xAxisLabel,
+      },
+      labels: {
+        rotation: -45,
+        style: {
+          fontSize: "13px",
+          fontFamily: "Verdana, sans-serif",
         },
-        title: {
-          text: 'Matches per City in FIFA Worldcup'
+      },
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: graphLabels[property].yAxisLabel,
+      },
+    },
+    legend: {
+      enabled: false,
+    },
+    tooltip: {
+      pointFormat: graphLabels[property].pointFormat,
+    },
+    series: [
+      {
+        name: "Population",
+        data: Object.entries(jsonData),
+        dataLabels: {
+          enabled: true,
+          rotation: -90,
+          color: "#FFFFFF",
+          align: "right",
+          format: "{point.y}",
+          y: 10,
+          style: {
+            fontSize: "10px",
+            fontFamily: "Verdana, sans-serif",
+          },
         },
-        xAxis: {
-          type: 'City',
-          labels: {
-            rotation: -45,
-            style: {
-              fontSize: '13px',
-              fontFamily: 'Verdana, sans-serif'
-            }
-          }
-        },
-        yAxis: {
-          min: 0,
-          title: {
-            text: 'No. of Matches'
-          }
-        },
-        legend: {
-          enabled: false
-        },
-        tooltip: {
-          pointFormat: 'Matches held: <b>{point.y}</b>'
-        },
-        series: [{
-          name: 'Population',
-          data: Object.entries(jsonData),
-          dataLabels: {
-            enabled: true,
-            rotation: -90,
-            color: '#FFFFFF',
-            align: 'right',
-            format: '{point.y}', 
-            y: 10,
-            style: {
-              fontSize: '10px',
-              fontFamily: 'Verdana, sans-serif'
-            }
-          }
-        }]
-      });
+      },
+    ],
+  });
 }
